5d6006b04cedfd4d9ff8d024f2bce59132ff6102
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-19 17:56:52 -0500

    html routes created

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+// HTML ROUTES 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/notes.html'))
+})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
 app.listen(PORT, () => {
