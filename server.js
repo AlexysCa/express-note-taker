e8072e3748c11e3180b133fabdc308cd66d560ac
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-01-21 12:52:56 -0500

    reworking code to uncover functionality error

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const app = express();
 const { v4: uuidv4 } = require('uuid');
+const PORT = process.env.PORT || 3001;
 
+// path to db files to put saved notes
+const userNotes = require('./Develop/db/db.json');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-const { json } = require('express/lib/response');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.static(__dirname + '/Develop/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(userNotes.slice(1));
+})
 
 // HTML ROUTES 
 app.get('/notes', (req, res) => {
@@ -23,10 +28,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'./Develop/public/index.html'))
 })
 
-// API ROUTES
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/db/db.json'))
-})
 
 app.post('api/notes', (req, res) => {
     let saveNote = {
